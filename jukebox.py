93eba8615399b3cc3f5932b937ab99a7e833ffd6
Matthew R. Demicco <matt@demicco.net>
2024-01-20 17:28:39 -0500

    Initial commit

---
+++ jukebox.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3.8
+
+# jukebox.py - Upload and play HVSC SID files on Ultimate64 / UII+
+# Matthew R. Demicco - 1/20/2024
+# https://youtube.com/retrobitstv
+#
+# Note: this is just a proof of concept, there's minimal error checking!
+
+import requests, random, hashlib, pathlib, re, os, time, sys
+
+# Configuration Starts Here
+
+# IP address of your Ultimate64 or UII+
+ultimateIP = '192.168.1.233'
+
+# Local path where you have unzipped High Voltage SID Collection
+# Download - https://www.hvsc.c64.org/
+HVSCPath = '/home/armitage/retrobits/C64Music'
+
+# HVSC songs to play, one file (full path) per line
+# e.g. /home/armitage/retrobits/C64Music/MUSICIANS/J/Jammer/Gliding_Gladly_2SID.sid
+playlist = '/home/armitage/retrobits/playlist.txt'
+
+# End of Configuration
+
+apiUrl = 'http://' + ultimateIP + '/v1/runners:sidplay'
+
+# Load and randomize the playlist
+sidFiles = open(playlist).read().splitlines()
+random.shuffle(sidFiles)
+
+# Iterate through the random playlist
+for sidFile in sidFiles:
+
+    # Calculate MD5 hash of the next file to play
+    md5 = hashlib.md5(pathlib.Path(sidFile).read_bytes()).hexdigest()
+
+    # Locate the MD5 hash in the HVSC Songlengths file
+    with open(HVSCPath + '/DOCUMENTS/Songlengths.md5', 'r') as songLengths:
+        for line in songLengths.readlines():
+            if md5 in line:
+                songLength = line
+
+    # Extract time from the line we found (discard microseconds)
+    runTime = re.search('.*=(\d+):(\d+).*', songLength)
+    seconds = int(runTime.group(1)) * 60 + int(runTime.group(2))
+
+    # Inform the user
+    filePath = os.path.split(sidFile)
+    print('\nPlaying ' + filePath[1] + ' (' + runTime.group(1) + ':' + runTime.group(2) + ')')
+
+    # Make the REST API call to upload and play the next SID
+
+    postReq = {"file": open(sidFile, 'rb')}
+    try:
+        req = requests.post(apiUrl, files=postReq)
+        if(not(req.status_code >= 200 and req.status_code < 300)):
+             print("REST API Error: " + str(req.status_code))
+             continue
+
+    except requests.exceptions.RequestException as e:
+        print("Failure talking to Ultimate device: " + str(e))
+        continue
+
+    # Count down
+
+    for timeLeft in range(seconds, 0, -1):
+        sys.stdout.write('\r')
+        sys.stdout.write('{:2d} seconds remaining.'.format(timeLeft))
+        sys.stdout.flush()
+        time.sleep(1)
+
+    # Done, move to the next file
+
+# Done with all files
+print("No more files to play, all done!")
+
+# TODO
+# Stop playback immediately when timer expires, then load next track
+# Re-randomize playlist and start again when reaching the end
+# Catch SIGINT and stop playback before exiting
+# Implement a key to skip to the next track
+# Error checking when loading playlist, retriving HVSC data, etc.
